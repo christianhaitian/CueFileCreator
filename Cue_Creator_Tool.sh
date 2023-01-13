bcf980eaa8aa45499aaba1ceecc67c182f94288f
christianhaitian <mckman@comcast.net>
2023-01-12 19:15:50 -0500

    Update Cue_Creator_Tool.sh

--- Cue_Creator_Tool.sh
+++ Cue_Creator_Tool.sh
@@ -62,51 +62,43 @@ fi
 # Start oga_controls
 cd /${whichsd}/tools/CueFileCreator
 sudo kill -9 $(pidof oga_controls)
-sudo ./oga_controls Cue_Creator_Tool.sh $param_device > /dev/null 2>&1 &
+sudo ./oga_controls Cue_Creator_Too $param_device > /dev/null 2>&1 &
 
 # Verify dialog is installed and if not, install it
 dpkg -s "dialog" &>/dev/null
 if [ "$?" != "0" ]; then
   sudo apt update && sudo apt install -y dialog --no-install-recommends
   temp=$(grep "title=" /usr/share/plymouth/themes/text.plymouth)
-  if [[ $temp == *"ArkOS 351P/M"* ]]; then
-    #Make sure sdl2 wasn't impacted by the install of dialog for the 351P/M
-  fi
 fi
 
-# Welcome
- dialog --backtitle "CUE File Creator" --title "CUE File Creator Utility" \
-    --yesno "\nThe script will automate the creation of CUE files for all the .bin files found in various CD based roms path.\n
-	The main python script used by this tool was originally created by A Former User for Retropie. \n(https://retropie.org.uk/forum/topic/31984/psx-cue-file-maker-script)
- \n\n\nDo you want to proceed?" $height $width 2>&1 > /dev/tty1
-
-case $? in
-       1) sudo kill -9 $(pidof oga_controls)
-          sudo systemctl restart oga_events &
-          exit
-          ;;
-esac
-
+userExit() {
+  sudo kill -9 $(pidof oga_controls)
+  sudo systemctl restart oga_events &
+  dialog --clear
+  printf "\033c" > /dev/tty1
+  exit 0
+}
 
 function main_menu() {
-    local choice
+    local options=( "$whichsd/dreamcast" 1 "$whichsd/psx" 2 "$whichsd/segacd" 3 "$whichsd/pcfx" 4 )
 
     while true; do
-        choice=$(dialog --backtitle "CUE File Creator" --title " MAIN MENU " \
-            --ok-label OK --cancel-label Exit \
-            --menu "What CD based game system folders would you like scanned and CUE files created for?" 25 75 20 \
-            1 "${whichsd}/dreamcast" \
-            2 "${whichsd}/psx" \
-            3 "/${whichsd}/segacd" \
-            2>&1 > /dev/tty1)
+		selection=(dialog \
+		--backtitle "CUE File Creator" \
+		--title " MAIN MENU " \
+		--no-collapse \
+		--clear \
+        --cancel-label "Exit" \
+        --menu "What CD based game system folders would you like scanned and CUE files created for" $height $width 15 )
 
-        case "$choice" in
-            1) CueFileCreator.py "/$choice"  ;;
-            2) CueFileCreator.py "/$choice"  ;;
-            3) CueFileCreator.py "/$choice"  ;;
-            *)  break ;;
-        esac
-    done
+        choices=$("${selection[@]}" "${options[@]}" 2>&1 > /dev/tty1) || userExit
+
+        for choice in $choices; do
+          case $choice in
+			*) python3 CueFileCreator.py "/$choice/"  ;;
+          esac
+        done
+	done
 }
 
 # Main
