5318153202587f4c9a03bb8e6fbe6c12f79cefa3
christianhaitian <mckman@comcast.net>
2023-01-12 21:18:29 -0500

    Update Cue_Creator_Tool.sh

--- Cue_Creator_Tool.sh
+++ Cue_Creator_Tool.sh
@@ -2,13 +2,19 @@
 
 #IFS=';'
 
-sudo chmod 666 /dev/tty1
+ESUDO="sudo"
+if [ $? != 0 ]; then
+  ESUDO=""
+fi
+$ESUDO chmod 666 /dev/tty1
 export TERM=linux
 export XDG_RUNTIME_DIR=/run/user/$UID/
 printf "\033c" > /dev/tty1
 dialog --clear
+hotkey="Select"
 height="15"
 width="55"
+rm /dev/shm/CueFileCreator.log
 
 if [ -f "/opt/system/Advanced/Switch to main SD for Roms.sh" ]; then
   whichsd="roms2"
@@ -21,7 +27,7 @@ fi
 if [[ -e "/dev/input/by-path/platform-ff300000.usb-usb-0:1.2:1.0-event-joystick" ]]; then
   param_device="anbernic"
   if [ -f "/boot/rk3326-rg351v-linux.dtb" ] || [ $(cat "/storage/.config/.OS_ARCH") == "RG351V" ]; then
-    sudo setfont /usr/share/consolefonts/Lat7-Terminus20x10.psf.gz
+    $ESUDO setfont /usr/share/consolefonts/Lat7-Terminus20x10.psf.gz
     height="20"
     width="60"
   fi
@@ -34,7 +40,7 @@ elif [[ -e "/dev/input/by-path/platform-odroidgo2-joypad-event-joystick" ]]; the
   fi
 elif [[ -e "/dev/input/by-path/platform-odroidgo3-joypad-event-joystick" ]]; then
   param_device="ogs"
-  sudo setfont /usr/share/consolefonts/Lat7-Terminus20x10.psf.gz
+  $ESUDO setfont /usr/share/consolefonts/Lat7-Terminus20x10.psf.gz
   height="20"
   width="60"
   if [ "$(cat ~/.config/.OS)" == "ArkOS" ] && [ "$(cat ~/.config/.DEVICE)" == "RGB10MAX" ]; then
@@ -48,26 +54,26 @@ elif [[ -e "/dev/input/by-path/platform-singleadc-joypad-event-joystick" ]]; the
   #else
     param_device="rg503"
   #fi
-  sudo setfont /usr/share/consolefonts/Lat7-Terminus20x10.psf.gz
+  $ESUDO setfont /usr/share/consolefonts/Lat7-Terminus20x10.psf.gz
   height="20"
   width="60"
 else
   param_device="chi"
   hotkey="1"
-  sudo setfont /usr/share/consolefonts/Lat7-Terminus20x10.psf.gz
+  $ESUDO setfont /usr/share/consolefonts/Lat7-Terminus20x10.psf.gz
   height="20"
   width="60"
 fi
 
 # Start oga_controls
 cd /${whichsd}/tools/CueFileCreator
-sudo kill -9 $(pidof oga_controls)
-sudo ./oga_controls Cue_Creator_Too $param_device > /dev/null 2>&1 &
+$ESUDO kill -9 $(pidof oga_controls)
+$ESUDO ./oga_controls Cue_Creator_Too $param_device > /dev/null 2>&1 &
 
 # Verify dialog is installed and if not, install it
 dpkg -s "dialog" &>/dev/null
 if [ "$?" != "0" ]; then
-  sudo apt update && sudo apt install -y dialog --no-install-recommends
+  $ESUDO apt update && $ESUDO apt install -y dialog --no-install-recommends
   temp=$(grep "title=" /usr/share/plymouth/themes/text.plymouth)
 fi
 
@@ -78,37 +84,51 @@ fi
  \n\n\nDo you want to proceed?" $height $width 2>&1 > /dev/tty1
 
 case $? in
-       1) sudo kill -9 $(pidof oga_controls)
-          sudo systemctl restart oga_events &
+       1) $ESUDO kill -9 $(pidof oga_controls)
+          $ESUDO systemctl restart oga_events &
           exit
           ;;
 esac
 
 userExit() {
-  sudo kill -9 $(pidof oga_controls)
-  sudo systemctl restart oga_events &
+  $ESUDO kill -9 $(pidof oga_controls)
+  $ESUDO systemctl restart oga_events &
   dialog --clear
   printf "\033c" > /dev/tty1
   exit 0
 }
 
 function main_menu() {
-    local options=( "$whichsd/dreamcast" 1 "$whichsd/psx" 2 "$whichsd/segacd" 3 "$whichsd/pcfx" 4 )
+    local options=( "$whichsd/dreamcast" "" \
+                    "$whichsd/pcfx" "" \
+                    "$whichsd/psx" "" \
+                    "$whichsd/segacd" "" )
 
     while true; do
-		selection=(dialog \
-		--backtitle "CUE File Creator" \
-		--title " MAIN MENU " \
-		--no-collapse \
-		--clear \
-        --cancel-label "Exit" \
-        --menu "What CD based game system folders would you like scanned and CUE files created for" $height $width 15 )
+       selection=(dialog \
+        --backtitle "CUE File Creator" \
+        --title " MAIN MENU " \
+        --no-collapse \
+        --clear \
+        --cancel-label "$hotkey + Start to Exit" \
+        --menu "What CD based game system folders would you like scanned and CUE files created for?" $height $width 15 )
 
         choices=$("${selection[@]}" "${options[@]}" 2>&1 > /dev/tty1) || userExit
 
         for choice in $choices; do
           case $choice in
-			*) python3 CueFileCreator.py "/$choice/"  ;;
+			*) python3 CueFileCreator.py "/$choice/"  
+			   results=$(cat /dev/shm/CueFileCreator.log | uniq)
+			   if [[ ! -z $results ]]; then
+			     dialog --clear --backtitle "CUE File Creator" --title "The following CUE files have been created" \
+			     --clear --msgbox "$results" $height $width 2>&1 > /dev/tty1
+			     rm /dev/shm/CueFileCreator.log
+			   else
+			     dialog --clear --backtitle "CUE File Creator" --title "The following CUE files have been created" \
+			     --clear --msgbox "There were no CUE files needed to be created for /$choice/" $height $width 2>&1 > /dev/tty1
+			     rm /dev/shm/CueFileCreator.log
+			   fi
+			;;
           esac
         done
 	done
@@ -117,5 +137,5 @@ function main_menu() {
 # Main
 
 main_menu
-sudo kill -9 $(pidof oga_controls)
-sudo systemctl restart oga_events &
+$ESUDO kill -9 $(pidof oga_controls)
+$ESUDO systemctl restart oga_events &
